f9e37522162a9366532bc6222da5d54ee3715b58
Brian Takita <brian.takita@gmail.com>
2023-10-21 01:27:22 -0400

    Post__schema: + canonical_url

--- src/post/Post__schema.ts
+++ src/post/Post__schema.ts
@@ -15,7 +15,8 @@ export const Post__schema = z.object({
 	hero_image: z.string().optional(),
 	og_image: z.string().optional(),
 	tags: z.array(z.string()).default(["others"]),
-	draft: z.boolean().optional()
+	draft: z.boolean().optional(),
+	canonical_url: z.string().optional(),
 } as {
 	[k in keyof Post['data']]:ZodType<Post['data'][k]>
 })
