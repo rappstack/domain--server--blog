1e27d5e0085225e836264a52c616f233ac8e57fb
Brian Takita <brian.takita@gmail.com>
2023-11-08 19:55:02 -0500

    blog_server__ctx__new: - ∋ props_clean__van__new

--- src/ctx/blog_server__ctx__new.ts
+++ src/ctx/blog_server__ctx__new.ts
@@ -1,9 +1,9 @@
 import { blog__ctx__new } from '@btakita/domain--all--blog'
 import { Astro__set } from '@btakita/domain--server'
-import { props_clean__van__new, van__set } from '@ctx-core/vanjs'
+import { van__set } from '@ctx-core/vanjs'
 import { type APIContext, type AstroGlobal } from 'astro'
 import van from 'mini-van-plate/van-plate'
-import { type LogoImage, logo_image__set } from '../logo'
+import { logo_image__set, type LogoImage } from '../logo'
 import { type Site, site__set } from '../site'
 import { type Social, socials__set } from '../social'
 export function blog_server__ctx__new(
@@ -15,7 +15,7 @@ export function blog_server__ctx__new(
 	}
 ) {
 	const ctx = blog__ctx__new()
-	van__set(ctx, props_clean__van__new(van))
+	van__set(ctx, van)
 	Astro__set(ctx, Astro)
 	logo_image__set(ctx, logo_image)
 	site__set(ctx, site)
