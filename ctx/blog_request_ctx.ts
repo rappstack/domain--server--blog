e6b06ed2ba608c5e91902e4c30070a1019e0ed5b
Brian Takita <brian.takita@gmail.com>
2024-02-20 02:56:52 -0500

    ./ctx: blog_server_request_ctx__ensure_config_T→blog_request_ctx__ensure_config_T

--- ctx/blog_request_ctx.ts
+++ ctx/blog_request_ctx.ts
@@ -1,5 +1,5 @@
 import { post_mod_a1__set, type post_mod_T } from '@rappstack/domain--any--blog/post'
-import { logo_image__set, logo_image_T } from '@rappstack/domain--server/logo'
+import { logo_image__set, type logo_image_T } from '@rappstack/domain--server/logo'
 import { social_a1__set, type social_T } from '@rappstack/domain--server/social'
 import { type middleware_ctx_T } from 'rebuildjs/server'
 import { request_ctx__ensure } from 'relysjs/server'
@@ -15,7 +15,7 @@ export function blog_request_ctx__ensure(
 		blog_site,
 		social_a1,
 		post_mod_a1,
-	}:blog_server_request_ctx__ensure_config_T
+	}:blog_request_ctx__ensure_config_T
 ) {
 	const request_ctx = request_ctx__ensure(middleware_ctx, context)
 	logo_image__set(request_ctx, logo_image)
@@ -24,7 +24,7 @@ export function blog_request_ctx__ensure(
 	post_mod_a1__set(request_ctx, post_mod_a1)
 	return request_ctx
 }
-export type blog_server_request_ctx__ensure_config_T = {
+export type blog_request_ctx__ensure_config_T = {
 	logo_image?:logo_image_T
 	blog_site:blog_site_T
 	social_a1:social_T[]
