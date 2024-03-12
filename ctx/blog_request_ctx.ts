38f6ef2d0ccd4ea6bdeee4749c747843636720d5
Brian Takita <brian.takita@gmail.com>
2024-03-12 19:07:49 -0400

    ./ctx:
    	blog_request_ctx__ensure:
    	blog_request_ctx__ensure_config_T:
    		- social_a1
    		- post_mod_a1
    	blog_request_ctx__ensure: ∋ post_mod_a1__set
    
    ./site: + site__post_mod_a1_

--- ctx/blog_request_ctx.ts
+++ ctx/blog_request_ctx.ts
@@ -1,9 +1,7 @@
-import { post_mod_a1__set, type post_mod_T } from '@rappstack/domain--any--blog/post'
-import { logo_image__new__set } from '@rappstack/domain--server/logo'
-import { social_a1__set, type social_T } from '@rappstack/domain--server/social'
+import { post_mod_a1__set } from '@rappstack/domain--any--blog/post'
 import { type middleware_ctx_T } from 'rebuildjs/server'
 import { request_ctx__ensure } from 'relysjs/server'
-import { blog_site__set, type blog_site_T } from '../site/index.js'
+import { blog_site__set, type blog_site_T, site__post_mod_a1_ } from '../site/index.js'
 export function blog_request_ctx__ensure(
 	middleware_ctx:middleware_ctx_T,
 	context:{
@@ -12,18 +10,13 @@ export function blog_request_ctx__ensure(
 	},
 	{
 		blog_site,
-		social_a1,
-		post_mod_a1,
 	}:blog_request_ctx__ensure_config_T
 ) {
 	const request_ctx = request_ctx__ensure(middleware_ctx, context)
 	blog_site__set(request_ctx, blog_site)
-	social_a1__set(request_ctx, social_a1)
-	post_mod_a1__set(request_ctx, post_mod_a1)
+	post_mod_a1__set(request_ctx, site__post_mod_a1_(request_ctx))
 	return request_ctx
 }
 export type blog_request_ctx__ensure_config_T = {
 	blog_site:blog_site_T
-	social_a1:social_T[]
-	post_mod_a1:post_mod_T[]
 }
--- site/blog_site.ts
+++ site/blog_site.ts
@@ -1,4 +1,5 @@
-import { site__set, type site_T } from '@rappstack/domain--server/site'
+import type { post_mod_T } from '@rappstack/domain--any--blog/post'
+import { site__set, type site_T, type social_T } from '@rappstack/domain--server/site'
 import { id_be_sig_triple_, memo_ } from 'relementjs'
 import { id_be_memo_pair_ } from 'rmemo'
 export const [
@@ -22,7 +23,15 @@ export const [
 ] = id_be_memo_pair_(
 	'site__home__post_count',
 	ctx=>blog_site_(ctx)?.home__post_count)
+export const [
+	,
+	site__post_mod_a1_
+] = id_be_memo_pair_(
+	'site__post_mod_a1',
+	ctx=>blog_site_(ctx)?.post_mod_a1)
 export type blog_site_T = site_T&{
+	social_a1:social_T[]
+	post_mod_a1:post_mod_T[]
 	page__post_count:number
 	home__post_count:number
 }
